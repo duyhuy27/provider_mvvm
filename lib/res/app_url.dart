f8817a804cb94f71ff3ad726ed1657f31d066153
Nguyen Hai Dang <dannynguyen.vnn@gmail.com>
2024-05-19 00:24:10 +0700

    update Environment

--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,6 +1,40 @@
-class AppUrl {
-  static var baseUrl = "https://reqres.in/";
+// ignore_for_file: constant_identifier_names
 
-  static var loginApiEndpoint = baseUrl + "api/login";
-  static var registerApiEndpoint = baseUrl + "api/register";
+enum Environment {
+  PRODUCTION('production'),
+  TEST('staging'),
+  DEV('development');
+
+  final String envEnum;
+  const Environment(this.envEnum);
+}
+
+class AppUrl {
+  static Environment get currentEnvironment => Environment.TEST;
+
+  static String get baseUrl {
+    switch (currentEnvironment) {
+      case Environment.PRODUCTION:
+        return 'https://reqres.in/';
+      case Environment.TEST:
+        return 'https://reqres.in/';
+      case Environment.DEV:
+        return 'https://reqres.in/';
+      default:
+        return 'https://reqres.in/';
+    }
+  }
+
+  static String _urlHandle(String url) =>
+      (url.contains('http') ? '' : baseUrl) + url;
+
+  static String loginApiEndpoint = _urlHandle("api/login");
+  static String registerApiEndpoint = _urlHandle("api/register");
+  static String getUserApiEndpoint(int id) => _urlHandle("api/users/$id");
+}
+
+class ContentType {
+  static const Application_Json = 'application/json';
+  static const URL_Encoded = 'application/x-www-form-urlencoded';
+  static const Multipart = 'multipart/form-data';
 }
